a5e06ed2c2dab8d49a3ca6ea0474efde6f40e1f1
youn1575 <youn461515@gmail.com>
2023-10-19 22:15:20 +0900

    07-2

---
+++ 07-2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) 
+	{
+	int i;
+	int temp = 1;
+	for (i=0;i<5;i++)
+	{
+		printf("temp = %d\n", temp);
+		temp++;
+	}
+	return 0;
+	}
