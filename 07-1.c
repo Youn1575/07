5519bea0fb7af405a8214d33b807dc340414573c
youn1575 <youn461515@gmail.com>
2023-10-19 15:20:04 +0900

    07-1

---
+++ 07-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	int flag = 1;
+	int y = 3; 
+	while(flag != 0)
+	{
+		int y;
+		flag = 0; 
+	}
+	y = 4; 
+	return 0;
+}
